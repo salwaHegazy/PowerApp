a4e4cbf72aaee519604b9d461849e2dc06f387f6
Salwa <salwa@Salwas-Mac.local>
2018-05-01 04:13:40 +0200

    power App

--- PowerAssignment/ViewController.swift
+++ PowerAssignment/ViewController.swift
@@ -10,16 +10,103 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var baseNumberTextField: UITextField!
+    
+    
+    @IBOutlet weak var powerNumberTextField: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+  
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
+    @IBAction func calculatePowerButton(_ sender: Any) {
+        
+          getPowerWithRecursion()    // with recursion *****
+        
+     //   getPowerWithLoop()        // with loop *****
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+func validateTextFields(){
+    
+    if (baseNumberTextField.text?.isEmpty)!{
+    
+    alertLabel.text = "Enter Base number "
+    resultLabel.text = ""
+    
+    }else if (powerNumberTextField.text?.isEmpty)!{
+    
+    alertLabel.text = "Enter Power number "
+    resultLabel.text = ""
+    
+    }else if (baseNumberTextField.text?.isEmpty)! && (powerNumberTextField.text?.isEmpty)!{
+    
+    alertLabel.text = "Enter Base and Power number "
+    resultLabel.text = ""
+    
+    } else
+    
+    {
+    
+    alertLabel.text = ""
+    
     }
+    
+}
 
-
+    
+    func getPowerWithRecursion() {
+    
+         validateTextFields()
+        
+        let baseNumber  = Double (baseNumberTextField.text!)!
+        let powerNumber  = Double (powerNumberTextField.text!)!
+        let power  =  (pow(baseNumber, powerNumber))
+        
+        resultLabel.text = String (power)
+        alertLabel.text = "power is - number "
+        
+        if (powerNumber >= 0 )
+        {
+             alertLabel.text = ""
+            resultLabel.text = String (power)
+           
+       }
+    
+    }
+    
+    
+    func getPowerWithLoop(){
+        
+        validateTextFields()
+     
+        let baseNumber  = Double (baseNumberTextField.text!)!
+        let powerNumber  = Double (powerNumberTextField.text!)!
+        let power  =  (pow(baseNumber, powerNumber))
+        
+        let p = powerNumber
+        
+        
+        while (p >= 0.0 )
+            
+        {
+            resultLabel.text = String (power)
+            
+        }
+     
+        alertLabel.text = "power is - number"
+        resultLabel.text = String (power)
+    }
+ 
 }
 
